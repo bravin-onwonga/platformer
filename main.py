5905a5cb4a76b7c1469ae54699d0f3a765765aa6
bravin-onwonga <onbravin@gmail.com>
2022-12-07 19:25:24 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+import os
+from pygame.locals import *
+
+pygame.init()
+
+screen_width = 1000
+screen_height = 700
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Platformer')
+
+
+#load images
+sun_img = pygame.image.load('img/sun.png')
+bg_img = pygame.image.load('img/sky.png')
+
+run = True
+while run:
+
+    screen.blit(bg_img, (0,0))
+    screen.blit(sun_img, (100,100))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    pygame.display.update()
+
+pygame.quit()
